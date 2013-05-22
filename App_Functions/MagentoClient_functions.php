d6fafad276d61582ee5a9812503a258dc75b3763
Hugo Zonderland <info@hugozonderland.nl>
2013-05-22 23:00:41 +0200

    Used for connecting to the Magento API and fetching + parsing data.

---
+++ App_Functions/MagentoClient_functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hugozonderland
+ * Date: 08-05-13
+ * Time: 17:31
+ * To change this template use File | Settings | File Templates.
+ */
+
+$mClient = new MagentoClient($settings->magento_api_username, $settings->magento_api_key, $settings->magento_api_url);
+?>
