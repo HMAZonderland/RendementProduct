e62a6e16d16654aaef71b050e19b39162f7d259d
Hugo Zonderland <info@hugozonderland.nl>
2013-05-22 22:31:24 +0200

    Added the Google API PHP Client.

---
+++ App_Config/Google_API_Client_config.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hugozonderland
+ * Date: 22-05-13
+ * Time: 22:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+// Google Console Credentials
+define('ACCESS_TYPE',       'offline');
+define('APPLICATION_NAME',  'esser-emmerik Rendement berekening API');
+define('CLIENT_ID',         '460555925231.apps.googleusercontent.com');
+define('CLIENT_SECRET',     '7PVI5jc-ukcl9Kh9vAw7nLM9');
+define('REDIRECT_URI',      'http://' . $_SERVER["HTTP_HOST"] . $_SERVER['PHP_SELF']);
+define('DEVELOPER_KEY',     'AIzaSyA-hXmL-SvFwZAyLLksMn9r9AyZfPGZKH4');
+
+?>
