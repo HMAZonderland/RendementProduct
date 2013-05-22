7f5f8e785d4a0d7b7306e9c9e951eb467ab8e37d
Hugo Zonderland <info@hugozonderland.nl>
2013-05-22 22:57:32 +0200

    Created mappings to html, css, javascript and website parts.

--- App_Config/Var_config.php
+++ App_Config/Var_config.php
@@ -7,6 +7,14 @@
  * To change this template use File | Settings | File Templates.
  */
 
-define('WEBSITE_URL',         'http://product.esser-emmerik.hugozonderland.nl/');
+define('WEBSITE_URL',       'http://product.esser-emmerik.hugozonderland.nl/');
 define('DOCUMENT_ROOT',     '/home/ocrtxndf/domains/hugozonderland.nl/public_html/product.esser-emmerik');
+
+// Includes made from the browser side
+define('CSS_ROOT',          WEBSITE_URL . 'Static/css/');
+define('IMAGE_ROOT',        WEBSITE_URL . 'Static/images/');
+define('JAVASCRIPT_ROOT',   WEBSITE_URL . 'Static/javascript/');
+
+// Includes made from the server/code side
+define('HTML_ROOT',         DOCUMENT_ROOT . 'Static/html/');
 ?>
---
+++ View/Static/html/nav.html.php
@@ -0,0 +1,26 @@
+<body class="page page-id-49 page-template-default theme1 ie">
+<header class="onerow color2">
+    <div class="onepcssgrid-1200">
+        <div class="col4 iconic icon-ok">
+            <div class="title">
+                <a href="http://www.presteren.nu">esser-emmerik</a>
+            </div>
+            <div>online verkoop experts</div>
+            <div class="phone icon-phone" onclick="location.href='tel:0357009703';">
+                <span><a href="tel:0357009703">035 - 7009 703</a></span>
+            </div>
+        </div>
+        <nav class="col8 last">
+            <div>
+                <ul id="mainnav">
+                    <li id="menu-item-48"><a href="<?= $_SERVER['PHP_SELF'] ?><?= isset($_REQUEST['id']) ? "?id=".$_REQUEST['id']: "";?>" onclick="clickDay();">Dag niveau</a></li>
+                    <li id="menu-item-288"><a href="<?= $_SERVER['PHP_SELF'] ?><?= isset($_REQUEST['id']) ? "?id=".$_REQUEST['id']: "";?>" onclick="clickWeek();">Week niveau</a></li>
+                    <li id="menu-item-288"><a href="<?= $_SERVER['PHP_SELF'] ?><?= isset($_REQUEST['id']) ? "?id=".$_REQUEST['id']: "";?>" onclick="clickMonth();">Maand niveau</a></li>
+                    <li id="menu-item-48"><a href="settings.php">Instellingen</a></li>
+                </ul>
+                <i class="selnav icon-align-justify"></i>
+            </div>
+        </nav>
+        <div class="arrow"></div>
+    </div>
+</header>
