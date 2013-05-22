e511efcdf42673a99b418bdd0d2e9540975eb20c
Hugo Zonderland <info@hugozonderland.nl>
2013-05-22 22:41:30 +0200

    Added a config file for database creditenials.

--- App_Config/db_config.php
+++ App_Config/db_config.php
@@ -6,3 +6,10 @@
  * Time: 15:54
  * To change this template use File | Settings | File Templates.
  */
+
+//Database settings
+define('DB_HOST',       '');
+define('DB_NAME',       '');
+define('DB_USERNAME',   '');
+define('DB_PASS',       '');
+?>
