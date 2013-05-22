7c18912307d2c31f2a0982c80752621384049a24
Hugo Zonderland <info@hugozonderland.nl>
2013-05-23 00:01:18 +0200

    Creates a connection to the database using the db_config.php file.

---
+++ Controller/_DatabaseController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hugozonderland
+ * Date: 22-05-13
+ * Time: 23:34
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * Class DatabaseController
+ *
+ * Handles all database communication using RedBeansPHP library
+ */
+class DatabaseController
+{
+    /**
+     * Sets up the connection to the database
+     */
+    public function __construct()
+    {
+        R::setup('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . '', '' . DB_USERNAME . '', '' . DB_PASS . '');
+    }
+}
