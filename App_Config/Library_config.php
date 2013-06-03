2460a9252f8797079bce6534c5b41fef2c109609
Hugo Zonderland <info@hugozonderland.nl>
2013-06-03 12:58:07 +0200

    Updated the library.

--- App_Config/Library_config.php
+++ App_Config/Library_config.php
@@ -8,11 +8,12 @@
  */
 
 define ('LIBRARY', serialize(array(
-    'Google_Analytics_Metrics_Parser' => LIBRARY_ROOT . 'Google_Analytics_Metrics_Parser/1.0/GoogleAnalyticsMetricsParser.php',
-    'Google_API_Client'               => LIBRARY_ROOT . 'Google_API_Client/0.6.2/Google_Client.php',
-    'Google_Analytics_Service'        => LIBRARY_ROOT . 'Google_API_Client/0.6.2/contrib/Google_AnalyticsService.php',
-    'Google_Oauth2Service'            => LIBRARY_ROOT . 'Google_API_Client/0.6.2/contrib/Google_Oauth2Service.php',
-    'MagentoClient'                   => LIBRARY_ROOT . 'MagentoClient/1.0/MagentoClient.php',
-    'RedBeanPHP'                      => LIBRARY_ROOT . 'RedBeanPHP/3.4/rb.php'
+    'Google_Analytics_Metrics_Parser'       => LIBRARY_ROOT . 'Google_Analytics_Metrics_Parser/1.0/GoogleAnalyticsMetricsParser.php',
+    'Google_API_Client'                     => LIBRARY_ROOT . 'Google_API_Client/0.6.2/Google_Client.php',
+    'Google_Analytics_Service'              => LIBRARY_ROOT . 'Google_API_Client/0.6.2/contrib/Google_AnalyticsService.php',
+    'Google_Oauth2Service'                  => LIBRARY_ROOT . 'Google_API_Client/0.6.2/contrib/Google_Oauth2Service.php',
+    'MagentoClient'                         => LIBRARY_ROOT . 'MagentoClient/1.0/MagentoClient.php',
+    'RedBeanPHP'                            => LIBRARY_ROOT . 'RedBeanPHP/3.4/rb.php',
+    'OrderPerMarketingChannel_Metrics'      => LIBRARY_ROOT . 'Google_Analytics_Metrics_Parser/1.0/metrics/OrderPerMarketingChannel.metrics.php'
 )));
 ?>
