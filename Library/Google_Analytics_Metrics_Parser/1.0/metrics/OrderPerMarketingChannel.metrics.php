e49360b40bcca4d91e33b51fde288fce0fc782f7
Hugo Zonderland <info@hugozonderland.nl>
2013-06-06 13:59:23 +0200

    Removed debug.

--- Library/Google_Analytics_Metrics_Parser/1.0/metrics/OrderPerMarketingChannel.metrics.php
+++ Library/Google_Analytics_Metrics_Parser/1.0/metrics/OrderPerMarketingChannel.metrics.php
@@ -44,9 +44,6 @@ class OrderPerMarketingChannel extends GoogleAnalyticsMetricsParser
      */
     public function getOrdersPerChannel()
     {
-        Debug::p($this->_data);
-        Debug::p($this->_results);
-
         $result = array();
         $source = null;
         $transactionId = null;
